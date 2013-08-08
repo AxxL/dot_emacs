1bafc39d53a6859215c5b0fc9cc0bfecfed625fb
Axel Schaefer <https://github.com/AxxL>
2013-08-08 17:29:56 +0200

    pretty print xml function and new snippets for jsapi 3.6
    pretty print xml function, new snippets in html mode, additional
    snippets in js-mode.

--- init.el
+++ init.el
@@ -1,3 +1,36 @@
+;; TODO ORG-MODE Keys
+; auto-complete-mode
+; local-unset-key S-tab
+; local-unset-key tab
+; local-set-key S-tab org-cycle
+
+
+;; pretty print xml region
+(defun pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+    (nxml-mode)
+    (goto-char begin)
+    ;; split <foo><foo> or </foo><foo>, but not <foo></foo>
+    (while (search-forward-regexp ">[ \t]*<[^/]" end t)
+      (backward-char 2) (insert "\n") (incf end))
+    ;; split <foo/></foo> and </foo></foo>
+    (goto-char begin)
+    (while (search-forward-regexp "<.*?/.*?>[ \t]*<" end t)
+      (backward-char) (insert "\n") (incf end))
+    (indent-region begin end nil)
+    (normal-mode))
+  (message "All indented!"))
+
+
+
+
+
 ;; empty kill ring manually
 ;; M-: (setq kill-ring nil)
 (add-to-list 'load-path "~/.emacs.d/")
