02b53e459fa681c714ba46fc19dfb4861ea3f8ed
AxxL <s-axel@gmx.de>
2015-03-18 22:52:51 +0100

    ausgelagert, company mode saveplace

--- init.el
+++ init.el
@@ -63,53 +63,18 @@
 (load "~/.emacs.d/lisp/keys.el")
 ;; (load "~/.emacs.d/init_emacs_js.el")
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;
 ;; SMEX
 ;;
 (smex-initialize)
 
-
-;;
-;; nyan-mode
-;;
-(require 'nyan-mode)
-(nyan-mode)
-
-;;
-;; mapserver-mode
-;;
-(add-to-list 'load-path "~/.emacs.d/lisp/mapserver-emacs-mode/")
-(autoload 'mapserver-mode "mapserver-mode" "Mode for editing UMN MapServer files." t)
-(add-to-list 'auto-mode-alist '("\\.map\\'" . mapserver-mode))
-
-;;
-;; window size keys
-;;
-(global-set-key (kbd "s-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "s-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "s-<down>") 'shrink-window)
-(global-set-key (kbd "s-<up>") 'enlarge-window)
-
 ;;
 ;; yaml mode
 ;;
 (require 'yaml-mode)
-
-
-;;
-;; F5 kills buffer
-;;
-(global-set-key (kbd "<f5>") 'ido-kill-buffer)
-
-;;
-;; f6 join-line
-;;
-(global-set-key (kbd "<f6>") 'join-line)
-
-;;
-;; dead-circumflex ( ) dired-up-directory
-;;
-(global-set-key (kbd "<dead-circumflex>") 'dired-up-directory)
 
 ;;
 ;; simple wiki mode
@@ -119,58 +84,34 @@
 ;;
 ;; Emacs Server
 ;;
-(server-start)
-
-;;
-;; Emacs Tramp
-;;
-(setq tramp-default-method "ssh")
-
-
-;;
-;; WEB-MODE
-;;
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode))
-
-
-
-;;
-;; GNUS
-;;
-;; GNUS can't send email error
-(add-to-list 'exec-path "/usr/bin/")
-
-
-;;
-;; Org Clocking?
-;; http://www.orgmode.org/manual/Clocking-work-time.html
-;;
-(setq org-clock-persist 'history)
-(org-clock-persistence-insinuate)
-
+                                        ;(server-start)
 
 ;;
 ;; AUTO-COMPLETE
 ;;
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(ac-config-default)
-(global-auto-complete-mode t)
+;; (require 'auto-complete-config)
+;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+;; (ac-config-default)
+;; (global-auto-complete-mode t)
 
 ;;
-;; YASNIPPET
-;; installed by M-x list-packkages
+;; company mode
 ;;
-(require 'yasnippet)
-(yas-global-mode 1)
-;; Let's have snippets in the auto-complete dropdown
-(add-to-list 'ac-sources 'ac-source-yasnippet)
+(global-company-mode t)
+(setq company-idle-delay 1)
+(setq company-selection-wrap-around t)
+;; remove annoying blinking
+;; (setq company-echo-delay 0)
+;; start autocompletion only after typing
+;; (setq company-begin-commands '(self-insert-command))
+;; Do not convert to lowercase
+;; (setq company-dabbrev-downcase nil)
 
+
+;;
+;; saveplace
+;; Remember the cursor position of files when reopening them
+;;
+(setq save-place-file "~/.emacs.d/saveplace")
+(setq-default save-place t)
+(require 'saveplace)
