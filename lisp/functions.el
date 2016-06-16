32316609db05989c4414db8d15df4f09d76ffedf
AxxL <s-axel@gmx.de>
2016-06-16 09:28:53 +0200

    toggle-window-split function

--- lisp/functions.el
+++ lisp/functions.el
@@ -210,3 +210,32 @@ by using nxml's indentation rules."
   (interactive)
   (setq buffer-display-table (make-display-table))
   (aset buffer-display-table ?\^M []))
+
+
+;; toggles between vertically splitting or horizontally splitting windows (if
+;; only 2 are open)
+;; https://www.reddit.com/r/emacs/comments/4dgxd6/weekly_emacs_questions_thread/d1s4hcx
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
