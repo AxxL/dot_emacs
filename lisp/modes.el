4818d52dbae72e50a250347de719e79b65ffb903
AxxL <s-axel@gmx.de>
2017-10-20 17:17:27 +0200

    Added tramp again

--- lisp/modes.el
+++ lisp/modes.el
@@ -19,12 +19,12 @@
 ;;
 ;; Tramp
 ;;
-;; (require 'tramp)
-;; (defun sudo-find-file (file-name)
-;;   "Like find file, but opens the file as root."
-;;   (interactive "FSudo Find File: ")
-;;   (let ((tramp-file-name (concat "/sudo::" (expand-file-name file-name))))
-;;     (find-file tramp-file-name)))
+(require 'tramp)
+(defun sudo-find-file (file-name)
+  "Like find file, but opens the file as root."
+  (interactive "FSudo Find File: ")
+  (let ((tramp-file-name (concat "/sudo::" (expand-file-name file-name))))
+    (find-file tramp-file-name)))
 ;; ssh
 (setq tramp-default-method "ssh")
 
