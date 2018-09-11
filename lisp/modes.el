90e615773b7c3a55366f01f454218d1a62f2165e
AxxL <s-axel@gmx.de>
2018-09-11 09:42:49 +0200

    A lot of new modes from init

--- lisp/modes.el
+++ lisp/modes.el
@@ -6,10 +6,14 @@
 (add-to-list 'auto-mode-alist '("\\.map\\'" . mapserver-mode))
 
 
+
+
 ;;
 ;; Projectile
 ;;
-;; (projectile-global-mode)
+(projectile-global-mode)
+(setq projectile-keymap-prefix (kbd "C-c C-c"))
+
 
 ;; Highlight current line
 ;; (global-hl-line-mode 1)
@@ -64,13 +68,13 @@
 ;; flx-ido
 ;; https://github.com/lewang/flx
 ;;
-;; (require 'flx-ido)
-;; (ido-mode 1)
-;; (ido-everywhere 1)
-;; (flx-ido-mode 1)
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
 
 ;; disable ido faces to see flx highlights.
-;; (setq ido-use-faces nil)
+(setq ido-use-faces nil)
 
 ;;
 ;; Speedbar, sr-speedbar
@@ -105,6 +109,32 @@
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+;; flymd
+;; https://github.com/mola-T/flymd/blob/master/browser.md
+(defun my-flymd-browser-function (url)
+  (let ((browse-url-browser-function 'browse-url-firefox))
+    (browse-url url)))
+(setq flymd-browser-open-function 'my-flymd-browser-function)
+
+;; Markdown Preview Mode (verwende ich nicht mehr. flymd ist besser)
+;; https://github.com/ancane/markdown-preview-mode
+;; (setq markdown-preview-stylesheets (list "http://thomasf.github.io/solarized-css/solarized-light.min.css"))
+;; (setq markdown-preview-stylesheets (list "~/.emacs.d/lisp/solarized-light.min.css"))
+
+;; Hooks
+(defun setup-markdown-mode()
+  (flyspell-mode 1)
+  (flyspell-buffer)
+  (visual-line-mode 1))
+
+(add-hook 'markdown-mode-hook 'setup-markdown-mode)
+
+
+;; RST Mode
+(add-hook 'rst-mode-hook 'setup-markdown-mode)
+
+
+
 ;;
 ;; WEB-MODE
 ;;
@@ -118,6 +148,26 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.twig?\\'" . web-mode))
+
+
+;; indent tab-width
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+
+;;
+;; yaml mode
+;;
+(require 'yaml-mode)
+
+
+;;
+;; simple wiki mode
+;;
+(require 'simple-wiki)
 
 
 ;; ,----
@@ -140,3 +190,25 @@
 ;;
 ;; (require 'nyan-mode)
 ;; (nyan-mode)
+
+;;
+;; calfw
+;;
+(require 'calfw)
+
+;;
+;; company mode
+;;
+(global-company-mode t)
+(setq company-idle-delay 0.2)
+(setq company-selection-wrap-around t)
+
+;;
+;; PDF Tools
+;;
+(pdf-tools-install)
+
+
+;; company mode camel case
+;; https://emacs.stackexchange.com/questions/10837/how-to-make-company-mode-be-case-sensitive-on-plain-text
+(setq company-dabbrev-downcase nil)
